f5a5fba1325bc53ad5e68f08dcb13d0b9b84a917
Anton <202008162@post.au.dk>
2022-03-09 15:21:01 +0100

    Testproject added

---
+++ HandIn2Ladeskab/HanIn2LadeskabTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HanIn2LadeskabTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
